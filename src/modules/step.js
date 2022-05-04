0cfe5d519c164bbbbb5ac93b03faba42e6d50eb0
lorof <gilyazov@list.ru>
2022-05-04 10:32:29 +0300

    fix

--- src/modules/step.js
+++ src/modules/step.js
@@ -40,23 +40,23 @@ function getTourProps(props: Object): Object {
 export function getMergedStep(step: StepProps, props: JoyrideProps): ?StepProps {
   if (!step) return null;
 
-  const mergedStep = deepmerge.all([getTourProps(props), DEFAULTS.step, step], {
+  const mergedStep = deepmerge.all([DEFAULTS.step, step, getTourProps(props)], {
     isMergeableObject: is.plainObject,
   });
-  const mergedStyles = getStyles(deepmerge(props.styles || {}, step.styles || {}));
+  const mergedStyles = getStyles(deepmerge(step.styles || {}, props.styles || {}));
   const scrollParent = hasCustomScrollParent(
     getElement(step.target),
     mergedStep.disableScrollParentFix,
   );
   const floaterProps = deepmerge.all([
-    props.floaterProps || {},
     DEFAULTS.floaterProps,
     mergedStep.floaterProps || {},
+    props.floaterProps || {},
   ]);
 
   // Set react-floater props
   floaterProps.offset = mergedStep.offset;
-  floaterProps.styles = deepmerge(floaterProps.styles || {}, mergedStyles.floaterStyles || {});
+  floaterProps.styles = deepmerge(mergedStyles.floaterStyles || {}, floaterProps.styles || {});
 
   delete mergedStyles.floaterStyles;
 
